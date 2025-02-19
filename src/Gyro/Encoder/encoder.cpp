a01d0ce260ed2217f2727dda444d5e45ae164cef
fergus-xu <116698024+fergus-xu@users.noreply.github.com>
2025-02-18 18:24:20 -0800

    Update encoder.cpp

--- src/Gyro/Encoder/encoder.cpp
+++ src/Gyro/Encoder/encoder.cpp
@@ -12,7 +12,7 @@ encoder::encoder(PinName A, PinName B){
 void encoder::setup(){
     pinA.mode(PullUp);
     pinB.mode(PullUp);
-    lastState = (static_cast<uint8_t>(_pinA.read()) << 1) | _pinB.read();
+    lastState = (static_cast<uint8_t>(pinA.read()) << 1) | pinB.read();
 
     pinA.fall(callback(this, &encoder::interruptA()));
     pinA.rise(callback(this, &encoder::interruptA()));
@@ -21,7 +21,7 @@ void encoder::setup(){
 }
 int16_t encoder::getPos(){
     __disable_irq();
-    int32_t pos = _position;
+    int32_t pos = position;
     __enable_irq();
     return pos;
 }
@@ -37,7 +37,7 @@ void encoder::reset(){
     __disable_irq();
     position = 0;
     direction = 0;
-    __enable_irq()
+    __enable_irq();
 }
 
 float encoder::getAngleDeg(){
@@ -60,15 +60,15 @@ void encoder::interruptA(){
 }
 
 void encoder::interruptB(){
-    uint8_t currentA = _pinA.read();
-    uint8_t currentB = _pinB.read();
+    uint8_t currentA = pinA.read();
+    uint8_t currentB = pinB.read();
     uint8_t state = (currentA << 1) | currentB;
     processState(state);
 }
 
 void encoder::processState(uint8_t state){
-    uint8_t prev = _lastState;
-    _lastState = state;
+    uint8_t prev = lastState;
+    lastState = state;
 
     int8_t diff = (prev << 2) | state;
     static const int8_t lookup[16] = {
@@ -81,7 +81,7 @@ void encoder::processState(uint8_t state){
     int8_t movement = lookup[diff & 0x0F];
 
     if (movement != 0) {
-        _position += movement;
-        _direction = movement;
+        position += movement;
+        direction = movement;
     }
 }
