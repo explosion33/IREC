61564122044ba42a67ad6aa38345528ac29e1b5b
fergus-xu <fergusx@gmail.com>
2025-02-28 13:38:53 -0800

    bno055 tests

---
+++ src/Gyro/BNO055/bno_test.cpp
@@ -0,0 +1,108 @@
+#include "bno055_test.h"
+
+BNO055Test::BNO055Test(BNO055* sensor, USBSerial* serial) {
+    this->sensor = sensor;
+    this->pc = serial;
+}
+
+void BNO055Test::print_status(const char* test_name, bool passed) {
+    pc->printf("[%s] %s\n", passed ? "PASS" : "FAIL", test_name);
+}
+
+void BNO055Test::test_set_get_OPMode() {
+    char expected_mode = BNO055_OPERATION_MODE_IMU;
+    sensor->setOPMode(expected_mode);
+    ThisThread::sleep_for(10ms);
+    char actual_mode = sensor->getOPMode();
+    print_status("OP Mode Test", actual_mode == expected_mode);
+}
+
+void BNO055Test::test_set_get_PWRMode() {
+    PWRMode expected_mode = PWRMode::LowPower;
+    sensor->setPWR(expected_mode);
+    ThisThread::sleep_for(10ms);
+    char actual_mode;
+    sensor->readData(BNO055_PWR_MODE, &actual_mode, 1);
+    print_status("Power Mode Test", actual_mode == expected_mode);
+}
+
+void BNO055Test::test_set_get_ACCConfig() {
+    char expected_config = 0x0D;
+    sensor->setACC(0x04, 0x02, 0x01);
+    ThisThread::sleep_for(10ms);
+    char actual_config;
+    sensor->readData(BNO055_ACC_CONFIG, &actual_config, 1);
+    print_status("Accelerometer Config Test", actual_config == expected_config);
+}
+
+void BNO055Test::test_set_get_GYROConfig() {
+    char expected_config = 0x07;
+    sensor->setGYR(0x03, 0x02, 0x02);
+    ThisThread::sleep_for(10ms);
+    char actual_config;
+    sensor->readData(BNO055_GYRO_CONFIG_0, &actual_config, 1);
+    print_status("Gyroscope Config Test", actual_config == expected_config);
+}
+
+void BNO055Test::test_set_get_MAGConfig() {
+    char expected_config = 0x01;
+    sensor->setMAG(0x01, 0x00, 0x00);
+    ThisThread::sleep_for(10ms);
+    char actual_config;
+    sensor->readData(BNO055_MAG_CONFIG, &actual_config, 1);
+    print_status("Magnetometer Config Test", actual_config == expected_config);
+}
+
+void BNO055Test::test_set_get_Axes() {
+    sensor->setAxes(Axes::Z, Axes::X, Axes::Y, false, false, false);
+    ThisThread::sleep_for(10ms);
+    char actual_axes;
+    sensor->readData(BNO055_AXIS_MAP_CONFIG, &actual_axes, 1);
+    char expected_axes = 0b00101000;
+    print_status("Axis Mapping Test", actual_axes == expected_axes);
+}
+
+void BNO055Test::test_set_get_TemperatureSource() {
+    sensor->writeData(BNO055_TEMP_SOURCE, 0x01, 1);
+    ThisThread::sleep_for(10ms);
+    char actual_source;
+    sensor->readData(BNO055_TEMP_SOURCE, &actual_source, 1);
+    print_status("Temperature Source Test", actual_source == 0x01);
+}
+
+void BNO055Test::test_set_get_UnitConfig() {
+    sensor->setUnit(true, false, true, false, true);
+    ThisThread::sleep_for(10ms);
+    char actual_config;
+    sensor->readData(BNO055_UNIT_SEL, &actual_config, 1);
+    char expected_config = 0b00010101;
+    print_status("Unit Config Test", actual_config == expected_config);
+}
+
+void BNO055Test::test_set_get_SysTrigger() {
+    sensor->writeData(BNO055_SYS_TRIGGER, 0x10, 1);
+    ThisThread::sleep_for(10ms);
+    char actual_value;
+    sensor->readData(BNO055_SYS_TRIGGER, &actual_value, 1);
+    print_status("System Trigger Test", actual_value == 0x10);
+}
+
+void BNO055Test::test_reset() {
+    sensor->reset();
+    ThisThread::sleep_for(700ms);
+    char sys_status = sensor->get_SysStatus();
+    print_status("Software Reset Test", sys_status == 0x01);
+}
+
+void BNO055Test::run_all_tests() {
+    test_set_get_OPMode();
+    test_set_get_PWRMode();
+    test_set_get_ACCConfig();
+    test_set_get_GYROConfig();
+    test_set_get_MAGConfig();
+    test_set_get_Axes();
+    test_set_get_TemperatureSource();
+    test_set_get_UnitConfig();
+    test_set_get_SysTrigger();
+    test_reset();
+}
---
+++ src/Gyro/BNO055/bno_test.h
@@ -0,0 +1,33 @@
+#ifndef BNO055_TEST_H
+#define BNO055_TEST_H
+
+#include "mbed.h"
+#include "bno055.h"
+#include "USBSerial.h"
+
+class BNO055Test {
+public:
+    // Constructor
+    BNO055Test(BNO055* sensor, USBSerial* serial);
+
+    // Test Functions
+    void test_set_get_OPMode();
+    void test_set_get_PWRMode();
+    void test_set_get_ACCConfig();
+    void test_set_get_GYROConfig();
+    void test_set_get_MAGConfig();
+    void test_set_get_Axes();
+    void test_set_get_TemperatureSource();
+    void test_set_get_UnitConfig();
+    void test_set_get_SysTrigger();
+    void test_reset();
+    void run_all_tests();
+
+private:
+    BNO055* sensor;
+    USBSerial* pc;
+
+    void print_status(const char* test_name, bool passed);
+};
+
+#endif // BNO055_TEST_H
