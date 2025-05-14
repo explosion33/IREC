dcc24f0659fefeb09638ce8f41380cf6d95fe9c0
fergus-xu <fergusx@gmail.com>
2025-05-13 22:27:31 -0700

    Optimizing for sensor rate

--- src/Gyro/main.cpp
+++ src/Gyro/main.cpp
@@ -28,6 +28,16 @@ encoder e1 (PA_8, PA_9, 4096);
 // encoder e2 (PA_8, PA_9, 4096);
 
 // TODO: figure out operating frequencies to get system fully functional with no errors
+/* 
+Maximums: 
+Acc - 100Hz
+Mag - 20Hz
+Gyro - 100Hz
+Fusion Data - 100Hz
+TMP - 8Hz
+*/
+
+// TODO: Need to overwrite defaults to run sensors faster
 
 // watchdog stuff
 Watchdog &watchdog = Watchdog::get_instance();
